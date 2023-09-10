a9cbfecf638a0e4e6ee5985c0db9e0b1cbbbb312
marjunstudio <uebujie@gmail.com>
2023-09-11 07:29:56 +0900

    add: 各エンドポイントのルーターを追加

--- api/routers/done.py
+++ api/routers/done.py
@@ -3,11 +3,11 @@ from fastapi import APIRouter
 router = APIRouter()
 
 # タスクに「完了フラグ」を立てる
-@router.put("/tasks/{task_id}/done")
-async def mark_task_as_done():
-  pass
+@router.put("/tasks/{task_id}/done", response_model=None)
+async def mark_task_as_done(task_id: int):
+  return
 
 # タスクの「完了フラグ」を削除
-@router.delete("/tasks/{task_id}_done")
-async def unmark_task_as_done():
-  pass
+@router.delete("/tasks/{task_id}_done", response_model=None)
+async def unmark_task_as_done(task_id: int):
+  return
