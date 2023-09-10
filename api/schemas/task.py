464f7a5b9bac85e708962492b8e765d752f2f462
marjunstudio <uebujie@gmail.com>
2023-09-10 22:23:59 +0900

    add: schemaを追加し、型を定義

---
+++ api/schemas/task.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel, Field
+
+class Task(BaseModel):
+  id: int
+  title: str | None = Field(None, example="クリーニングを取りに行く")
+  done: bool = Field(False, description="完了フラグ")
