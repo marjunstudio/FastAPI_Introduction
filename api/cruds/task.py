0593fc17a6dbc7fc30cd0e76b52a77335d17c94e
marjunstudio <uebujie@gmail.com>
2023-09-24 18:06:25 +0900

    fix: deleteを非同期化

--- api/cruds/task.py
+++ api/cruds/task.py
@@ -36,9 +36,9 @@ async def update_task(
   original.title  = task_create.title
   db.add(original)
   await db.commit()
-  await ßdb.refresh(original)
+  await db.refresh(original)
   return original
 
-def delete_task(db: Session, original: task_model.Task) -> None:
-  db.delete(original)
-  db.commit()
+async def delete_task(db: AsyncSession, original: task_model.Task) -> None:
+  await db.delete(original)
+  await db.commit()
--- api/routers/task.py
+++ api/routers/task.py
@@ -30,9 +30,9 @@ async def update_task(task_id: int, task_body: task_schema.TaskCreate, db: Async
 
 # タスクの削除
 @router.delete("/tasks/{task_id}", response_model=None)
-async def delete_task(task_id: int, db: Session = Depends(get_db)):
-  task = task_crud.get_task(db, task_id=task_id)
+async def delete_task(task_id: int, db: AsyncSession = Depends(get_db)):
+  task = await task_crud.get_task(db, task_id=task_id)
   if task is None:
     raise HTTPException(status_code=404, detail="Task not found")
   
-  return task_crud.delete_task(db, original=task)
+  return await task_crud.delete_task(db, original=task)
